401fea1b1bf3d982240f6e45ec733c4060991dfa
karri-durgaprasad <prasadkka2002@gmail.com>
2026-07-18 17:52:24 +0530

    Updated Durga and Rahul and Priya

--- Durga.java
+++ Durga.java
@@ -1,7 +1,9 @@
 public class Durga { 
          static Rahul r = new Rahul();
+         static Priya p = new Priya();
          public static void main(String[] args) {
                System.out.println("durga's file , this is the main method");
                r.m1();
+               p.m1();
          }
 }
